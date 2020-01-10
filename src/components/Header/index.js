302f632f1796a7a3af5cf04cb9d617c82be9f063
Maria Eduarda Spiess <maria.spiess@senior.com.br>
2020-01-10 12:04:45 -0300

    teste heroku

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -39,7 +39,7 @@ export class Header extends Component {
                             </li>
                         </ul>
                     </div>
-                </nav>
+                </nav> 
             </header>
         )
     }
