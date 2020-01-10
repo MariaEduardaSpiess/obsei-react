d9f61fdfdad26550746c4f69d3307c3bca3870d1
Maria Eduarda Spiess <maria.spiess@senior.com.br>
2020-01-10 17:51:41 -0300

    aba de projetos

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:8080'});
+// const api = axios.create({ baseURL: 'http://localhost:8080'});
+const api = axios.create({ baseURL: 'https://portal-obsei-api.herokuapp.com/'});
 
 export default api;
